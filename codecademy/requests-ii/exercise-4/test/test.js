43d947e9712ace932ee9076e9a03b2ec3d8d95fe
Sam O <isphinxs@hotmail.com>
2020-05-15 22:30:02 -0400

    Finishes Exercise 4 on Requests II
    Finishes exercise 4 with all tests passing.

---
+++ codecademy/requests-ii/exercise-4/test/test.js
@@ -0,0 +1,20 @@
+console.log = function() {};
+const assert = require('chai').assert;
+const fs = require('fs');
+const Structured = require('structured');
+
+const code = fs.readFileSync('public/main.js', 'utf8');
+
+describe('', function () {
+  it('', function() {
+    let structureOne = function() {
+      const getSuggestions = () => {
+        fetch(endpoint).then(response => {})
+      }
+    };
+
+
+    let isMatchOne = Structured.match(code, structureOne);
+    assert.isOk(isMatchOne, 'Did you chain `.then` to `fetch()` and include `response` as a parameter?');
+  });
+});
